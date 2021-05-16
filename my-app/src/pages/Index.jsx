ae14cadc5e74f4ba5e5a531de35b7b96e129e135
Bruce <767961652@qq.com>
2021-05-16 23:31:43 +0800

    build an item by react

---
+++ my-app/src/pages/Index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// export default class Index extends React.Component {
+
+//     render(){
+//         return(
+//             <>
+//                 <h1>首页</h1>
+//             </>
+//         )
+//     }
+// }
+
+const Index = () => {
+    return (
+        <>
+            <h1>首页</h1>
+        </>
+    )
+}
+
+export default Index;
